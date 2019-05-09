854caab6c58fb16a88f53394c099a923f42616fa
Serban <serban@gmail.com>
2019-05-09 00:01:42 -0700

    Ports PtrJumpIterationLoop from PtrJumpFunctor
    This is a first attempt. Look for SDP and remaining <TODO> items.

--- gunrock/app/cc/cc_enactor.cuh
+++ gunrock/app/cc/cc_enactor.cuh
@@ -415,6 +415,178 @@ struct HookMaxIterationLoop : public IterationLoopBase
  * @tparam EnactorT Type of enactor
  */
 template <typename EnactorT>
+struct PtrJumpIterationLoop : public IterationLoopBase
+    <EnactorT, Use_FullQ | Push
+    // <TODO>if needed, stack more option, e.g.:
+    // | (((EnactorT::Problem::FLAG & Mark_Predecessors) != 0) ?
+    // Update_Predecessors : 0x0)
+    // </TODO>
+    >
+{
+    typedef typename EnactorT::VertexT VertexT;
+    typedef typename EnactorT::SizeT   SizeT;
+    typedef typename EnactorT::ValueT  ValueT;
+    typedef typename EnactorT::Problem::GraphT::CsrT CsrT;
+    typedef typename EnactorT::Problem::GraphT::GpT  GpT;
+    
+    typedef IterationLoopBase
+        <EnactorT, Use_FullQ | Push
+        // <TODO> add the same options as in template parameters here, e.g.:
+        // | (((EnactorT::Problem::FLAG & Mark_Predecessors) != 0) ?
+        // Update_Predecessors : 0x0)
+        // </TODO>
+        > BaseIterationLoop;
+
+    helloIterationLoop() : BaseIterationLoop() {}
+
+    /**
+     * @brief Core computation of hello, one iteration
+     * @param[in] peer_ Which GPU peers to work on, 0 means local
+     * \return cudaError_t error message(s), if any
+     */
+    cudaError_t Core(int peer_ = 0)
+    {
+        // --
+        // Alias variables
+        
+        auto &data_slice = this -> enactor ->
+            problem -> data_slices[this -> gpu_num][0];
+        
+        auto &enactor_slice = this -> enactor ->
+            enactor_slices[this -> gpu_num * this -> enactor -> num_gpus + peer_];
+        
+        auto &enactor_stats    = enactor_slice.enactor_stats;
+        auto &graph            = data_slice.sub_graph[0];
+        auto &frontier         = enactor_slice.frontier;
+        auto &oprtr_parameters = enactor_slice.oprtr_parameters;
+        auto &retval           = enactor_stats.retval;
+        auto &iteration        = enactor_stats.iteration;
+        
+        auto &component_ids = data_slice.component_ids;
+        auto &vertex_flag   = data_slice.vertex_flag;
+        // </TODO>
+        
+        // --
+        // Define operations
+
+        // advance operation
+        auto advance_op = [
+            // </TODO>
+        ] __host__ __device__ (
+            const VertexT &src, VertexT &dest, const SizeT &edge_id,
+            const VertexT &input_item, const SizeT &input_pos,
+            SizeT &output_pos) -> bool
+        {
+            // SDP, not really sure what to return here? Use 'true' for now.
+            return true;
+            // </TODO>
+        };
+
+        // filter operation
+        auto filter_op = [
+            component_ids,
+            vertex_flag
+            // </TODO>
+        ] __host__ __device__ (
+            const VertexT &src, VertexT &dest, const SizeT &edge_id,
+            const VertexT &input_item, const SizeT &input_pos,
+            SizeT &output_pos) -> bool
+        {
+            VertexId parent         = Load<cub::LOAD_CG>(component_ids + src);
+            VertexId grand_parent   = Load<cub::LOAD_CG>(component_ids + parent);
+        
+            if (parent != grand_parent) 
+            {
+
+                Store(0, vertex_flag + 0);
+                Store(grand_parent, component_ids + src);
+            }
+
+            return true;
+            // </TODO>
+        };
+        
+        // --
+        // Run
+        
+        // <TODO> some of this may need to be edited depending on algorithmic needs
+        // !! How much variation between apps is there in these calls?
+        
+        GUARD_CU(oprtr::Advance<oprtr::OprtrType_V2V>(
+            graph.csr(), frontier.V_Q(), frontier.Next_V_Q(),
+            oprtr_parameters, advance_op, filter_op));
+        
+        if (oprtr_parameters.advance_mode != "LB_CULL" &&
+            oprtr_parameters.advance_mode != "LB_LIGHT_CULL")
+        {
+            frontier.queue_reset = false;
+            GUARD_CU(oprtr::Filter<oprtr::OprtrType_V2V>(
+                graph.csr(), frontier.V_Q(), frontier.Next_V_Q(),
+                oprtr_parameters, filter_op));
+        }
+
+        // Get back the resulted frontier length
+        GUARD_CU(frontier.work_progress.GetQueueLength(
+            frontier.queue_index, frontier.queue_length,
+            false, oprtr_parameters.stream, true));
+
+        // </TODO>
+        
+        return retval;
+    }
+
+    /**
+     * @brief Routine to combine received data and local data
+     * @tparam NUM_VERTEX_ASSOCIATES Number of data associated with each transmition item, typed VertexT
+     * @tparam NUM_VALUE__ASSOCIATES Number of data associated with each transmition item, typed ValueT
+     * @param  received_length The numver of transmition items received
+     * @param[in] peer_ which peer GPU the data came from
+     * \return cudaError_t error message(s), if any
+     */
+    template <
+        int NUM_VERTEX_ASSOCIATES,
+        int NUM_VALUE__ASSOCIATES>
+    cudaError_t ExpandIncoming(SizeT &received_length, int peer_)
+    {
+        
+        // ================ INCOMPLETE TEMPLATE - MULTIGPU ====================
+        
+        auto &data_slice    = this -> enactor ->
+            problem -> data_slices[this -> gpu_num][0];
+        auto &enactor_slice = this -> enactor ->
+            enactor_slices[this -> gpu_num * this -> enactor -> num_gpus + peer_];
+        //auto iteration = enactor_slice.enactor_stats.iteration;
+        // TODO: add problem specific data alias here, e.g.:
+        // auto         &distances          =   data_slice.distances;
+
+        auto expand_op = [
+        // TODO: pass data used by the lambda, e.g.:
+        // distances
+        ] __host__ __device__(
+            VertexT &key, const SizeT &in_pos,
+            VertexT *vertex_associate_ins,
+            ValueT  *value__associate_ins) -> bool
+        {
+            // TODO: fill in the lambda to combine received and local data, e.g.:
+            // ValueT in_val  = value__associate_ins[in_pos];
+            // ValueT old_val = atomicMin(distances + key, in_val);
+            // if (old_val <= in_val)
+            //     return false;
+            return true;
+        };
+
+        cudaError_t retval = BaseIterationLoop:: template ExpandIncomingBase
+            <NUM_VERTEX_ASSOCIATES, NUM_VALUE__ASSOCIATES>
+            (received_length, peer_, expand_op);
+        return retval;
+    }
+}; // end of PtrJumpIterationLoop
+
+/**
+ * @brief defination of hello iteration loop
+ * @tparam EnactorT Type of enactor
+ */
+template <typename EnactorT>
 struct helloIterationLoop : public IterationLoopBase
     <EnactorT, Use_FullQ | Push
     // <TODO>if needed, stack more option, e.g.:
